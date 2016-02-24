ac66e68c0262819c59abf52cb0c81aa63812877b
Side Li <lisided@gmail.com>
2016-02-24 12:36:23 -0800

    Merged codes

--- classifier.py
+++ classifier.py
@@ -31,27 +31,33 @@ def perceptron(dataset,w):
 			w= np.add(w, np.multiply(label, a))
 	return w
 
-def votedPecptron(dataset):
+def votedPecptron(dataset, w, num):
 	c = 1
-	w = np.array([0] * 784)
 	setCW = []
-	for vector in dataset:
-		label = vector[-1]
-		if(label == 0 ):
-			label = -1
-		else:
-			label = 1
-		a = np.array(vector[:-1])
-		if( np.multiply(label, np.dot(a,w)) <= 0):
-			setCW.append([w,c])
-			w= np.add(w, np.multiply(label, a))
-			c = 1
-		else:
-			c = c+1
+	count = 0
+
+	for i in range(num):
+		for vector in dataset:
+			label = vector[-1]
+			if(label == 0 ):
+				label = -1
+			else:
+				label = 1
+			a = np.array(vector[:-1])
+			if( np.multiply(label, np.dot(w,a)) <= 0):
+				setCW.append([w,c])
+				w= np.add(w, np.multiply(label, a))
+				c = 1
+			else:
+				c = c+1
+			count += 1
+	setCW.append([w,c-1])
+	#count += c
+
 	return  setCW
 
 def VotedClassifer(VPec, testingData):
-	sign = 0
+	#sign = 0
 	result = []
 	for testData in testingData:
 		sign = 0
@@ -60,8 +66,6 @@ def VotedClassifer(VPec, testingData):
 				sign += VC[1]
 			else:
 				sign += -VC[1]
-
-		print sign
 		if(sign >= 0 ): result.append([testData[:-1], 6])
 		else: result.append([testData[:-1], 0])
 	return result
@@ -69,13 +73,18 @@ def VotedClassifer(VPec, testingData):
 def AvgClassifer(APec, testingData):
 	result = []
 	WSum = [0] * 784
+
 	for VC in APec:
-		WSum += np.multiply(VC[:-1],VC[-1])
+		# VC[0] is w (featurs), VC[1] is y (labels)
+		WSum += np.multiply(VC[0],VC[1])
+
+
 	for testData in testingData:
 		if( np.dot(WSum, testData[:-1] ) >= 0):
 			result.append([testData[:-1], 6])
 		else:
 			result.append([testData[:-1], 0])
+
 	return result
 
 def PecClassfier(w, testingData):
@@ -87,22 +96,23 @@ def PecClassfier(w, testingData):
 			result.append([testData[:-1], 0])
 	return result
 
-# def getAccuary(testSet, predicts, conMatrix):
-# 	accuarcy = 0
-# 	for x in range(len(testSet)):
-# 		if testSet[x][-1] == predicts[x]:
-# 			accuarcy += 1
-# 		conMatrix[predicts[x]][testSet[x][-1]] +=1
-# 	return (accuarcy/float(len(testSet)))*100
-
 def OVAPecClassfier(w, testingData):
 	if (np.dot(w, testingData[:-1]) >= 0):
 		return 1
 	else:
 		return 0
 
-
 def OVAperceptron(dataset, w, l):
+	for vector in dataset:
+  		label = vector[-1]
+ 		if(label == l ):
+  			label = 1
+ 		else:
+ 			label = -1
+  		a = np.array(vector[:-1])
+  		if( np.multiply(label, np.dot(a,w)) <= 0):
+  			w= np.add(w, np.multiply(label, a))
+  	return w
 
 def onevsall():
 	loadData()
@@ -157,53 +167,21 @@ def ErrorTester(result, testData):
 	return float(errorNum)/float(SumNum)
 
 def main():
-
 	loadData()
-
 	w = np.array([0] * 784)
 	w = perceptron(trainingDataA, w)
 
-	setCW = votedPecptron(trainingDataA)
+	newW =  np.array([0] * 784)
+	setCW = votedPecptron(trainingDataA, newW, 1)
 
 	VotedResult = VotedClassifer(setCW, testingDataA)
-	#print VotedResult
+
 	AvgResult = AvgClassifer(setCW, testingDataA)
-	#print AvgResult
-	PecResult = PecClassfier(w, testingDataA)
 
-	print ErrorTester(PecResult, testingDataA)
-	# k = 3
-	# testSet = testingData
-	# predicts = []
-    #
-	# for i in range(len(testSet)):
-	# 	neighbors = getNeighbors(trainingData, testSet[i], k)
-	# 	label = getVote(neighbors)
-	# 	predicts.append(label)
-    #
-    #
-	# f = open('hw2test.txt', 'r')
-	# labels = [line.split()[-1] for line in f]
-    #
-    #
-	# countLabel = sorted(Counter(labels).most_common())
-	# matrix = [[0 for i in range(10)] for j in range(10)]
-    #
-	# accuarcy = getAccuary(testSet, predicts, matrix)
-    #
-	# print '   ',
-	# for i in range(len(matrix[i])):
-	# 	print '{:4}'.format(i),
-	# print ' '
-	# for i in range(len(matrix)):
-	# 	print (str(i)+"|"),
-	# 	print ' ',
-	# 	for j in range(len(matrix[i])):
-	# 		matrix[i][j] = float((matrix[i][j]) / float((countLabel[j])[1]))
-	# 		print '{:4.2f}'.format((matrix[i][j])),
-	# 	print ' '
-    #
-	# print("accuarcy: "+repr(accuarcy) +"%")
+	PecResult = PecClassfier(w, trainingDataA)
 
-#main()
-onevsall()
+	print ErrorTester(VotedResult,testingDataA)
+
+
+main()
+#onevsall()
